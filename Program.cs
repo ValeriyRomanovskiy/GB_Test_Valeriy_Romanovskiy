14b4bd72c58013f7db22f61df04134bdc5b43d00
maestro8690 <maestro8690@gmail.com>
2022-07-27 23:28:18 +0500

    create massive

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿String[] massive = { "123", "4567", "GB", "HelloWorld", "IT" };
+
+int count = 0;
+
+for (int i = 0; i < massive.Length; i++)
+{
+    char[] chars = massive[i].ToCharArray();
+
+    if (chars.Length <= 3)
+    {
+        count++;
+    }
+}
+
